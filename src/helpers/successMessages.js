22764aba7365519628fa1a38d0fc06d3b1ef67ce
Erico dos Santos Bispo <erico.desenvolvedor@gmail.com>
2024-03-19 21:25:41 -0300

    feat: create success messages

---
+++ src/helpers/successMessages.js
@@ -0,0 +1,6 @@
+const successMessages = {
+    subjectNewOrder: "Novo pedido solicitado",
+    bodyNewOrder: (name) => `Olá, ${name}! Seu pedido foi concluído com sucesso!!`
+};
+
+module.exports = successMessages;
